7249c0b40d4fb5347e44f369fd5a1d35ac0c8807
hydronics2 <hydronics@gmail.com>
2020-03-15 11:17:09 -0700

    new gerber view

--- arduino/Blink_IR_Leds/Blink_IR_Leds.ino
+++ arduino/Blink_IR_Leds/Blink_IR_Leds.ino
@@ -2,10 +2,18 @@
  * WARNING: this code turns LEDs on for a long time (1 second). 
  * This is fine if the resistors are installed and the jumpers are NOT made 
  * 
+ * 
+ * 
+ * 
 */
 
+//Pins Feather ESP32
 const byte powerGates1 = 15;
 const byte powerGates2 = 33;
+
+//Pins ItsyBitsy
+//const byte powerGates1 = 10;
+//const byte powerGates2 = 11;
 
 void setup ()
 {
